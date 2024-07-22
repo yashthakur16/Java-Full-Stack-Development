b5d23b71e2d3cf82d3e4d7dd43087ef2315fe700
yashthakur16 <tyash6843@gmail.com>
2024-07-22 22:12:06 +0530

    jpa implemented

--- Hib/HQL/src/main/java/com/yash/HQL/Jpa.java
+++ Hib/HQL/src/main/java/com/yash/HQL/Jpa.java
@@ -8,10 +8,21 @@ public class Jpa {
 
 	public static void main(String[] args) {
 
-		EntityManagerFactory emf= Persistence.createEntityManagerFactory(null);
+		pc p =new pc();
+		p.setBrand("Samsung");
+		p.setPrice(10000);
+		p.setLid(54);
+
+		EntityManagerFactory emf= Persistence.createEntityManagerFactory("pu");
 		EntityManager em = emf.createEntityManager();
 		
-		pc p = em.find(pc.class, 51);
+		// pc p = em.find(pc.class, 51);
+		
+		em.getTransaction().begin();
+		em.persist(p);
+		em.getTransaction().commit();
+
+		
 		
 		System.out.println(p);
 		
